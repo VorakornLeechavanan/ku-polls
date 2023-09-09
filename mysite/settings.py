656622171a100e88b099c81e28af4b83ab5759d1
VorakornLeechavanan <vorakorn.l@ku.th>
2023-09-09 18:17:01 +0700

    error commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os.path
 from pathlib import Path
+from decouple import config, Csv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%=o(%uk1d#s$b7k9kqm@qm^-%5v+p20=--9z3w%)r_f^nl1j(-"
+SECRET_KEY = config('SECRET_KEY', default="django-insecure-%=o(%uk1d#s$b7k9kqm@qm^-%5v+p20=--9z3w%)r_f^nl1j(-")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool, default=True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOST', cast=Csv(), default=[])
 
 
 # Application definition
@@ -108,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = config('TIME_ZONE', default='UTC')
 
 USE_I18N = True
 
